0b184f496553fc205091d4b1563c47d626acf80e
Jan Maly <jan@invenus.dev>
2023-01-04 13:20:35 +0100

    feat: 04 added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { container as container01 } from './mostly-adequate/01-what-ever-are-we-doing';
 import { container as container04 } from './mostly-adequate/04-currying';
+import { container as container05 } from './mostly-adequate/05-composing';
 import { section_logger } from './utils';
 
 container01(section_logger('01', 'What Ever Are We Doing?'));
 container04(section_logger('04', 'Currying'));
+container05(section_logger('05', 'Composing'));
--- src/mostly-adequate/05-composing.ts
+++ src/mostly-adequate/05-composing.ts
@@ -4,13 +4,13 @@ import { flow } from 'fp-ts/lib/function';
 import { reduce } from 'fp-ts/lib/ReadonlyArray';
 import { Monoid } from 'fp-ts/lib/string';
 
-export const container = () => {
-  // husbandry
+export const container = (logger: (...v: any) => void) => {
+  // Functional Husbandry
   const toUpperCase = (x: string) => x.toUpperCase();
   const exclaim = (x: string) => `${x}!`;
 
   const shout = flow(toUpperCase, exclaim);
-  console.log(shout('send in the clowns'));
+  logger(shout('send in the clowns'));
 
   const test_arr = ['jumpkick', 'roundhouse', 'uppercut'];
   const head = (x: any) => x[0];
@@ -23,17 +23,17 @@ export const container = () => {
       }
       return fn(a) > fn(b) ? 1 : -1;
     });
-  console.log(last(test_arr));
+  logger(last(test_arr));
 
   const transformer_1 = flow(flow(reverse, head), toUpperCase);
   const transformer_2 = flow(reverse, flow(head, toUpperCase));
-  console.log(transformer_1(test_arr), transformer_2(test_arr));
+  logger(transformer_1(test_arr), transformer_2(test_arr));
 
   const lastUpper = flow(reverse, head, toUpperCase);
   const loudLastUpper = flow(reverse, head, toUpperCase, exclaim);
-  console.log(lastUpper(test_arr), loudLastUpper(test_arr));
+  logger(lastUpper(test_arr), loudLastUpper(test_arr));
 
-  // pointfree
+  // Pointfree
   const replace =
     (search: string | RegExp) => (replace: string) => (s: string) =>
       s.replace(search, replace);
@@ -43,20 +43,21 @@ export const container = () => {
   const stringIntercalculate = (sep: string) => (s: string[]) =>
     intercalate(Monoid, Foldable)(sep, s);
   const trace = (tag: string) => (x: any) => {
-    console.log(tag, x);
+    logger(tag, x);
     return x;
   };
 
   const snakeCase = flow(toLower, replace(/\s+/gi)('_'));
-  console.log(snakeCase('Snake case test'));
+  logger(snakeCase('Snake case test'));
 
   const initials = flow(
     split(' '),
     map(flow(head, toUpper)),
     stringIntercalculate('. ')
   );
-  console.log(initials('hunter stock thompson'));
+  logger(initials('hunter stock thompson'));
 
+  // Debugging
   const dasherize = flow(
     split(' '),
     map(toLower),
@@ -64,9 +65,9 @@ export const container = () => {
     stringIntercalculate('-')
   );
 
-  console.log(dasherize('The world is a vampire'));
+  logger(dasherize('The world is a vampire'));
 
-  // examples
+  // Exercises
   const prop = (property: string) => (object: any) => object[property];
 
   type Car = {
@@ -91,12 +92,12 @@ export const container = () => {
   ];
 
   const isLastInStock = flow(last, prop('in_stock'));
-  console.log(isLastInStock(car_set));
+  logger(isLastInStock(car_set));
 
   const add = (a: number, b: number) => a + b;
   const average = (xs: number[]) => reduce(0, add)(xs) / xs.length;
   const averageDollarValue = flow(map(prop('dollar_value')), average);
-  console.log(averageDollarValue(car_set));
+  logger(averageDollarValue(car_set));
 
   const append = (s: string) => (appendOn: string) => appendOn + ' ' + s;
   const fastestCar = flow(
@@ -105,5 +106,5 @@ export const container = () => {
     prop('name'),
     append('is the fastest')
   );
-  console.log(fastestCar(car_set));
+  logger(fastestCar(car_set));
 };
