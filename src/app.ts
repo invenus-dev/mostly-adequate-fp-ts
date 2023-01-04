9f0c02652fb0db794663218c2ab3275b93fba70a
Jan Maly <jan@invenus.dev>
2023-01-04 11:01:26 +0100

    feat: added section logger

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
 import { container } from './mostly-adequate/08-tupperware';
 
-container();
+const section_logger = (section_string: string) => {
+  console.log(`===== logging for section: ${section_string}`);
+  return (value: any) => console.log(section_string, value);
+};
+
+container(section_logger('08'));
--- src/mostly-adequate/08-tupperware.ts
+++ src/mostly-adequate/08-tupperware.ts
@@ -15,14 +15,14 @@ const prop = (property: string) => (object: any) => object[property];
 const append = (string_to_add: string) => (original_string: string) =>
   original_string + ' ' + string_to_add;
 
-export const container = () => {
+export const container = (logger: (v: any) => void) => {
   // The Mighty Container
-  console.log(Container.of(3));
-  console.log(Container.of('hotdogs'));
-  console.log(Container.of(Container.of({ name: 'yoda' })));
+  logger(Container.of(3));
+  logger(Container.of('hotdogs'));
+  logger(Container.of(Container.of({ name: 'yoda' })));
 
   // My First Functor
-  console.log(Container.of('string').map((arg) => arg.length));
-  console.log(Container.of('flamethrowers').map((s) => s.toUpperCase()));
-  console.log(Container.of('bombs').map(append('away')).map(prop('length')));
+  logger(Container.of('string').map((arg) => arg.length));
+  logger(Container.of('flamethrowers').map((s) => s.toUpperCase()));
+  logger(Container.of('bombs').map(append('away')).map(prop('length')));
 };
